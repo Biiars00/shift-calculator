12030ad40e5c825a56ca8d098315a8896200ba4b
Biiars00 <biiaribeiro60@gmail.com>
2023-05-26 17:17:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+<h1 align="center">Shift Salculator</h1>
+
+![Shift Calculator](assets/plantao.gif)
+
+##  Sobre este projeto
+
+A **Shift Salculator** calcula os valores correspondentes aos plantões trabalhados. *Este projeto tem o intuito de facilitar os cálculos dos plantões realizados por trabalhadores que não têm acesso a um sistema corporativo (freelancers, cooperados etc.).*  
+
+##  Funcionalidades
+Os plantões são disponibilizados no formato 12h e 24h. 
+
+```
+Selecione o mês desejado:
+```
+
+```
+Selecione o plantão desejado (12h ou 24h):
+```
+
+```
+Informe o valor do plantão R$:
+```
+
+```
+Informe a quantidade de dias trabalhados no mês selecionado:
+```
+
+```
+EXEMPLO
+Resultado: 13 plantões de 12h realizados no mês de JUNHO correspondem a R$ 2080.00. 
+```
+
+### Tecnologias Utilizadas
+
+  <p display="inline-block">
+  <a href="https://skillicons.dev">
+    <img src="https://skillicons.dev/icons?i=py" />
+  </a>
+                   
+### Ferramentas de Desenvolvimento
+
+<p display="inline-block">
+  <img width="48" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2048px-Visual_Studio_Code_1.35_icon.svg.png" alt="vscode-logo"/>
+</p>
+
+## Referências
+
+Desenvolvido por: <a href="https://github.com/Biiars00" target="_blank"> Beatriz Ribeiro | Biiars00 </a>
---
+++ page.py
@@ -0,0 +1,46 @@
+#Calculadora de plantões
+def calculadora_plantao():
+    select_meses = input("Selecione o mês desejado............................................: ")
+    meses_do_ano = ('janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro')
+
+    mes = select_meses.upper()
+
+    if select_meses in meses_do_ano:
+        print(f'Você selecionou o mês de {mes}. ')
+
+        select_plantao = int(input("Selecione o plantão desejado........................................: "))
+        plantao = [12, 24]
+
+        if select_plantao in plantao:
+            print(f'Plantão {select_plantao}h selecionado. ')
+
+            if select_plantao == 12:
+                select_valor_12 = float(input("Informe o valor do plantão 12h......................................: "))
+                select_valor_12_format = "{:.2f}".format(select_valor_12)
+                print(f'O valor do plantão informado é de R$ {select_valor_12_format}. ')
+
+                dias_trabalhados_12 = int(input(f"Informe a quantidade de dias trabalhados no mês selecionado.........: "))
+                plantao_12 = dias_trabalhados_12 * select_valor_12
+                plantao_12_format = "{:.2f}".format(plantao_12)
+                print(f"{dias_trabalhados_12} plantões de 12h realizados no mês de {mes} correspondem a R$ {plantao_12_format}. ")
+
+            elif select_plantao == 24:
+                select_valor_24 = float(input("Informe o valor do plantão 24h......................................: "))
+                select_valor_24_format = "{:.2f}".format(select_valor_24)
+                print(f'O valor do plantão informado é de R$ {select_valor_24_format}. ')
+
+                dias_trabalhados_24 = int(input(f"Informe a quantidade de dias trabalhados no mês selecionado.........: "))
+                plantao_24 = dias_trabalhados_24 * select_valor_24
+                plantao_24_format = "{:.2f}".format(plantao_24)
+                print(f"{dias_trabalhados_24} plantões de 12h realizados no mês de {mes} correspondem a R$ {plantao_24_format}. ")
+
+            else:
+                print("Informe um valor correto! ")
+
+        else:
+            print("Informe uma opção correta! ")
+    else:
+        print("Informe uma opção correta!")
+        
+calcp = calculadora_plantao()
+
